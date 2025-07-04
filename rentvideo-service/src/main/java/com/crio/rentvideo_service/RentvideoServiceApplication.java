2c904e1012840bb48a1da4655719162d0467edd7
azkar7307 <azkar7307@gmail.com>
2025-07-04 17:20:13 +0530

    project setup completed

---
+++ rentvideo-service/src/main/java/com/crio/rentvideo_service/RentvideoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.crio.rentvideo_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentvideoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentvideoServiceApplication.class, args);
+	}
+
+}
